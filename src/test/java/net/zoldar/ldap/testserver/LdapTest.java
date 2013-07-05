1a2a3f28d8037c5431f7c0da9a73991aaf9893d7
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-07-05 11:29:53 +0200

    Modified test code to use new helpers.

--- src/test/java/net/zoldar/ldap/testserver/LdapTest.java
+++ src/test/java/net/zoldar/ldap/testserver/LdapTest.java
@@ -1,29 +1,33 @@
 package net.zoldar.ldap.testserver;
 
-import com.github.trevershick.test.ldap.annotations.LdapEntry;
+import com.github.trevershick.test.ldap.LdapServerResource;
+import com.github.trevershick.test.ldap.annotations.LdapAttribute;
+import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import com.github.trevershick.test.ldap.LdapServerResource;
-import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
-@LdapConfiguration(
-	bindDn = "cn=Directory Manager", 
-	password = "mypass", 
-	port = 11111, 
-	base = @LdapEntry(dn = "dc=myroot", objectclass = { "top", "domain" }))
 public class LdapTest {
     private LdapServerResource server;
 
     @Before
     public void startup() throws Exception {
-	    server = new LdapServerResource(this).start();
+        LdapConfiguration config = LdapBuilder.config()
+                .base(LdapBuilder.entry(
+                        "dc=myroot",
+                        new String[]{"top", "domain"},
+                        new LdapAttribute[]{}))
+                .bindDn("cn=Directory Manager")
+                .password("mypass")
+                .port(11111).build();
+        server = new LdapServerResourceCreator(config).getResource();
+        server.start();
     }
 
     @After
@@ -34,5 +38,6 @@ public class LdapTest {
     @Test
     public void basicServerTest() {
         assertTrue(server.isStarted());
+        assertEquals(server.port(), 11111);
     }
 }
