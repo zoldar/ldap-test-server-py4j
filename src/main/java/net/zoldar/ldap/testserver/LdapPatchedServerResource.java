b380fe74654fc6b2d6a189f5e86706e0f253d318
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-09-20 11:46:51 +0200

    Disabled schema validation to allow non-standard entries.

--- src/main/java/net/zoldar/ldap/testserver/LdapPatchedServerResource.java
+++ src/main/java/net/zoldar/ldap/testserver/LdapPatchedServerResource.java
@@ -1,7 +1,11 @@
 package net.zoldar.ldap.testserver;
 
 import com.github.trevershick.test.ldap.LdapServerResource;
+import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
 import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.DN;
 import com.unboundid.ldap.sdk.LDAPException;
 import com.unboundid.ldif.LDIFChangeRecord;
 import com.unboundid.ldif.LDIFReader;
@@ -14,8 +18,27 @@ public class LdapPatchedServerResource extends LdapServerResource {
     private InMemoryDirectoryServer server;
 
     protected InMemoryDirectoryServer configureWithPort(int port) throws LDAPException,BindException {
-        this.server = super.configureWithPort(port);
-        return server;
+        LdapConfiguration config = defaultConfiguration();
+
+        InMemoryListenerConfig listenerConfig = (port > 0) ?
+                InMemoryListenerConfig.createLDAPConfig("default", config.port()) :
+                InMemoryListenerConfig.createLDAPConfig("default");
+
+        InMemoryDirectoryServerConfig c = new InMemoryDirectoryServerConfig(new DN(config.base().dn()));
+        c.setListenerConfigs(listenerConfig);
+        c.addAdditionalBindCredentials(config.bindDn(), config.password());
+        c.setSchema(null);
+
+        server = new InMemoryDirectoryServer(c);
+        try {
+            server.startListening();
+            return server;
+        } catch (LDAPException ldapException) {
+            if (ldapException.getMessage().contains("java.net.BindException")) {
+                throw new BindException(ldapException.getMessage());
+            }
+            throw ldapException;
+        }
     }
 
     protected void loadLdifFiles() throws Exception {
